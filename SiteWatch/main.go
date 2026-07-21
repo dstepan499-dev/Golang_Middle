6f7a1c3577142b63fca13ffb6242c3e7963b51e0
s.druzhin <s.druzhin@g.nsu.com>
2026-07-21 23:33:37 +0700

    added channel for results

--- SiteWatch/main.go
+++ SiteWatch/main.go
@@ -6,25 +6,57 @@ import (
 	"sync"
 )
 
-func checkSite(url string, wg *sync.WaitGroup) {
+type Result struct {
+	URL string
+	Success bool
+	Message string
+}
+
+func checkSite(url string, results chan<- Result, wg *sync.WaitGroup) {
 	defer wg.Done()
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("[FAIL] %s (ошибка: %v)\n", url, err)
+		results <- Result{URL: url, Success: false, Message: err.Error()}
 		return
 	}
 	
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusOK {
-		fmt.Printf("[OK]  %s\n", url)
+		results <- Result{URL: url, Success: true, Message: "OK"}
 	} else {
-		fmt.Printf("[FAIL] %s (статус: %d)\n", url, resp.StatusCode)
+		results <- Result{URL: url, Success: false, Message: fmt.Sprintf("Status: %d", resp.StatusCode)}
 	}
 }
 
+//можно использовать локальную переменную, чтобы не делать results <- Result{...}
+/*
+var res Result
+res.URL = url
+resp, err := http.Get(url)
+    if err != nil {
+        res.Success = false
+        res.Message = "Ошибка: " + err.Error()
+        ch <- res // Отправляем и выходим
+        return
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode == http.StatusOK {
+        res.Success = true
+        res.Message = "OK"
+    } else {
+        res.Success = false
+        res.Message = fmt.Sprintf("Статус: %d", resp.StatusCode)
+    }
+
+    ch <- res // Отправляем финальный результат
+}
+*/
+
 func main() {
 	var wg sync.WaitGroup
+	results := make(chan Result)
 	sites := []string{
 		"https://google.com",
 		"https://github.com",
@@ -33,8 +65,15 @@ func main() {
 
 	for _, site := range sites {
 		wg.Add(1)
-		go checkSite(site, &wg)
+		go checkSite(site, results, &wg)
 	}
-	
-	wg.Wait()
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for res := range results {
+		fmt.Println(res)
+	}
 }
